5577990630fdd771c7bc98a66ac0ae7309175a3d
DESKTOP-DGFPD6H\SHIN HEEMIN <sinhimin11@naver.com>
2024-08-04 16:16:39 +0900

    Clustering Analysis: NON-HierarchicalClustering(KMeans)

---
+++ 18_Clustering Analysis_2.R
@@ -0,0 +1,27 @@
+# ===============================================
+#    Non-Hierarchical Clusterng : 비계층적 군집분석 
+# ===============================================
+# 처음부터 군집 갯수를 정해두고 최적의 Clustering 수행 
+
+# > Kmeans -> 'seeds' : 군집 생성 중심 points 
+
+# 데이터 준비 
+data(iris)
+newiris <- iris
+newiris$Species <- NULL 
+
+# 훈련(학습)
+kc <- kmeans(newiris,3) # 3개의 군집으로 나눔 
+
+# 결과 비교 
+table(iris$Species, kc$cluster) 
+# > 목적집단(Species)를 지정해두지 않고 특성값들의 거리로만 구분한 군집이 실제 집단(Species)와 얼마나 일치하는지 
+#             1  2  3
+# setosa      50 0 17
+# versicolor  0 2  48     --> 2: virginica/ 3 : versicolor
+# virginica   0 36  0
+
+
+# 시각화 
+plot(newiris[c("Sepal.Length", "Sepal.Width")], col = kc$cluster)
+#  두 개의 변수 사용하여 데이터를 색깔로 구분된 클러스터링 결과로 시각화
